1eefb0ee0b7a9c23918a91494ba0023212c5e526
Arief Bayu Purwanto <ariefbayu@gmail.com>
2016-08-31 08:44:43 +0700

    nambahin informasi getitem spinner

--- app/src/main/java/org/akupeduli/workshophari2/Day2F10Activity.java
+++ app/src/main/java/org/akupeduli/workshophari2/Day2F10Activity.java
@@ -3,6 +3,9 @@ package org.akupeduli.workshophari2;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import org.akupeduli.workshophari2.paketadapter.SpinGambarAdapter;
@@ -26,8 +29,19 @@ public class Day2F10Activity extends AppCompatActivity {
         listGambar.add(R.drawable.g3);
 
         TypedArray isiGambar = getResources().obtainTypedArray(R.array.definisi_gambar);
-        SpinGambarAdapter adapter = new SpinGambarAdapter(this, listGambar, isiGambar);
+        final SpinGambarAdapter adapter = new SpinGambarAdapter(this, listGambar, isiGambar);
         spinner.setAdapter(adapter);
+
+        ((Button)findViewById(R.id.button6)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = (String)spinner.getSelectedItem();
+                spinner.getSelectedItemId();
+                int position = spinner.getSelectedItemPosition();
+                String dataAdapter = (String)adapter.getItem(position);
+                Log.d("DAY2", data + " == " + dataAdapter);
+            }
+        });
 
 
     }
